ace2ec4706ab1fbee85ecf33b1ba72c101c22997
PydevG <peterndindi41@gmail.com>
2025-02-01 15:45:57 +0300

    commit 4

--- simpleblog/settings.py
+++ simpleblog/settings.py
@@ -39,10 +39,13 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "posts",
+    "accounts",
     #Third party apps
     'rest_framework',
     
 ]
+
+AUTH_USER_MODEL = 'accounts.User'
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
